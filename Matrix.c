0564fac6424175b78bfff3dd588e8162755c97bd
Lucia Rae Vazquez <lrv8295@rit.edu>
2025-11-02 23:30:38 -0500

    struct

--- Matrix.c
+++ Matrix.c
@@ -5,7 +5,7 @@
 #include "Matrix.h"
 
 Matrix mat_create(size_t rows, size_t cols) {
-	Matrix mat = malloc(sizeof(matrix_st));
+	Matrix mat = malloc(sizeof(struct matrix_st));
 	if (mat == NULL) {
 		return NULL;
 	}
